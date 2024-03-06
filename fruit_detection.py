b7fa024a3129492d86c5a7ff95a9ba13bd1faf7f
nsergun <110171634+nsergun@users.noreply.github.com>
2024-03-06 23:30:17 +0300

    added str(i) in dict part

--- fruit_detection.py
+++ fruit_detection.py
@@ -19,5 +19,5 @@ def fruit_detection(filename):
                           (int(result.boxes.xyxy[i][2]), int(result.boxes.xyxy[i][3])), (0, 255, 0), 2)
             cv2.putText(img_2, labels[int(cls)] + str(i), (int(result.boxes.xyxy[i][0]), int(result.boxes.xyxy[i][1])),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            img_label_results.append({"label": labels[int(cls)], "crop_img": crop_img})
+            img_label_results.append({"label": labels[int(cls)] + str(i), "crop_img": crop_img})
     return img_2, img_label_results
