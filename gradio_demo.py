2b194dae1b877fe264b43a05d2fb05296cb12654
Emre Çoban <emre02onur09@gmail.com>
2024-05-16 22:26:46 +0300

    Add files via upload

--- gradio_demo.py
+++ gradio_demo.py
@@ -11,61 +11,64 @@ from ultralytics import YOLO
 import cv2
 
 
+# Define the Regression Model class
 class RegressionModel(nn.Module):
-
     def __init__(self):
-
         super(RegressionModel, self).__init__()
+        # Load pretrained ResNet101
         resnet = torchvision.models.resnet101(pretrained=True)
-        self.features = nn.Sequential(
-            *list(resnet.children())[:-1]
-        )  # Remove the last fully connected layer
-        self.regressor1 = nn.Linear(
-            2048, 256
-        )  # Replace the last layer with a regression layer
-        self.regressor2 = nn.Linear(256, 1)
+        # Remove the last fully connected layer
+        self.features = nn.Sequential(*list(resnet.children())[:-1])
+        # Replace the last layer with regression layers
+        self.regressor1 = nn.Linear(2048, 512)
+        self.regressor2 = nn.Linear(512, 64)
+        self.regressor3 = nn.Linear(64, 1)
 
     def forward(self, x):
-
         x = self.features(x)
         x = x.view(x.size(0), -1)
         x = self.regressor1(x)
+        x = nn.GELU()(x)
         x = self.regressor2(x)
+        x = nn.GELU()(x)
+        x = self.regressor3(x)
         return x
 
 
-# Define the model
+# Load the pre-trained model state dictionary
+model_state_dict = torch.load("regression_model.pth", map_location="cpu")
+# Instantiate the RegressionModel
 model_reg = RegressionModel()
-
-
-model = torch.load("regression_model.pth", map_location="cpu")
+# Load the state dictionary into the model
+model_reg.load_state_dict(model_state_dict)
+# Set the model to evaluation mode
 model_reg.eval()
 
+# Define transformations for test images
 mean = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
-
 test_transforms = transforms.Compose(
     [
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=mean, std=std),
     ]
 )
 
 
+# Define the regression function
 def regression(image):
-
     img_numpy = test_transforms(Image.fromarray(image))
     image_tensor = img_numpy.unsqueeze(0)  # Add batch dimension
     with torch.no_grad():
         output = model_reg(image_tensor)
-    print(output.shape)
-    return output.squeeze().tolist()
+    return output.item()
 
 
+# Define the object detection function
 def hugg_face(img):
-
-    model = YOLO("plv7.pt")
+    # Load YOLO model
+    model = YOLO("yolo.pt")
     labels = [
         "freshpeach",
         "freshlemon",
@@ -74,13 +77,16 @@ def hugg_face(img):
         "freshmandarin",
         "rottenmandarin",
         "freshtomato",
-        "rottentomato"
+        "rottentomato",
+        "freshcucumber",
+        "rottencucumber",
     ]
     img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
     results = model(img)
     img_label_results = []
     img_2 = img.copy()
 
+    # Process each detection result
     for result in results:
         for i, cls in enumerate(result.boxes.cls):
             crop_img = img[
@@ -111,26 +117,23 @@ def hugg_face(img):
     img_2_pil = Image.fromarray(cv2.cvtColor(img_2, cv2.COLOR_BGR2RGB))
     regression_results = []
 
-    # Iterate over cropped images and their labels
+    # Perform regression on each cropped image
     for item in img_label_results:
         label = item["label"]
         cropped_img = item["crop_img"]
-        # Apply regression to the cropped image
         regression_output = regression(cropped_img)
-        # Append the regression output along with its label to the regression_results list
+        # Append regression results to the list
         regression_results.append(
-            {"label": label, "Rotten Proportion": regression_output}
+            {"label": label, "Rotten Part Percentage": round(regression_output,2)}
         )
     return img_2_pil, regression_results
 
 
-# Create Gradio interface
-inputs = gr.Image(type="pil")  # Define input image shape
-
-# Define output types: one for the text box (for regression results) and one for the image (segmented image)
+# Define Gradio interface
+inputs = gr.Image(type="pil")
 outputs = [
-    gr.Image(type="pil", label="Detecion Result"),
-    gr.Textbox(label="Regression Results"),
+    gr.Image(type="pil", label="Detection Result"),  # Output for the segmented image
+    gr.Textbox(label="Regression Results"),  # Output for the regression results
 ]
 
 app = gr.Interface(
